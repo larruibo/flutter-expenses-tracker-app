1f9c6c058c8683e862ec0907ebc6408b6c07b000
Luis Ruiz <la.ruiz967@uniandes.edu.co>
2023-06-26 00:17:20 -0500

    updates

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -28,11 +28,25 @@ class _ExpensesState extends State<Expenses> {
         amount: 8.99,
         date: DateTime.now(),
         category: Category.food),
+    Expense(
+      title: 'Office Supplies',
+      amount: 15.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
   ];
 
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () { print('Add expense');},
+          )],
+        title: const Text('Expenses Tracker'),
+      ),
       body: Column(
         children: [
           const Text('Chart'),
